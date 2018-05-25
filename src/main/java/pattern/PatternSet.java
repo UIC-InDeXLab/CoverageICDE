943905616c10076c6cd07b655921f9f07a12090c
Zhongjun Jin <markjin1990@gmail.com>
2018-05-25 15:26:57 -0400

    try using roaringbitmap; but it gets slower

--- src/main/java/pattern/PatternSet.java
+++ src/main/java/pattern/PatternSet.java
@@ -1,17 +1,22 @@
 package pattern;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import org.roaringbitmap.FastAggregation;
+import org.roaringbitmap.RoaringBitmap;
 
 import io.DataSet;
 
 public class PatternSet {
 	public Set<Pattern> patternSet;
 
-	public BitSet[][] patternBitVecForCheckingAncestor;
-	public BitSet[][] patternBitVecForCheckingDescendant;
+	public RoaringBitmap[][] patternBitVecForCheckingAncestor;
+	public RoaringBitmap[][] patternBitVecForCheckingDescendant;
 
 	public int[] patternBitVecForCheckingAncestorVectorLength;
 	public int[] patternBitVecForCheckingDescendantVectorLength;
@@ -27,12 +32,12 @@ public class PatternSet {
 		this.cardinalities = Arrays.copyOf(cardinalities,
 				cardinalities.length);;
 		this.patternSet = new HashSet<Pattern>();
-		this.patternBitVecForCheckingAncestor = new BitSet[cardinalities.length
+		this.patternBitVecForCheckingAncestor = new RoaringBitmap[cardinalities.length
 				+ 1][DataSet.sumOfArray(this.cardinalities)
 						+ this.cardinalities.length]; // there are
 														// this.cardinalities.length
 														// many "x"s
-		this.patternBitVecForCheckingDescendant = new BitSet[cardinalities.length
+		this.patternBitVecForCheckingDescendant = new RoaringBitmap[cardinalities.length
 				+ 1][DataSet.sumOfArray(this.cardinalities)
 						+ this.cardinalities.length]; // there are
 														// this.cardinalities.length
@@ -43,10 +48,8 @@ public class PatternSet {
 			for (int attrValue = 0; attrValue < DataSet
 					.sumOfArray(this.cardinalities)
 					+ this.cardinalities.length; attrValue++) {
-				this.patternBitVecForCheckingAncestor[level][attrValue] = new BitSet(
-						10);
-				this.patternBitVecForCheckingDescendant[level][attrValue] = new BitSet(
-						10);
+				this.patternBitVecForCheckingAncestor[level][attrValue] = new RoaringBitmap();
+				this.patternBitVecForCheckingDescendant[level][attrValue] = new RoaringBitmap();
 			}
 		}
 
@@ -82,7 +85,7 @@ public class PatternSet {
 				char curAttrValue = patternToAdd.data[attrId];
 				int rowId = checkRowIdxInPatternBitVec(attrId, curAttrValue);
 				this.patternBitVecForCheckingAncestor[currentLevel][rowId]
-						.set(patternId);
+						.add(patternId);
 			}
 		}
 
@@ -94,7 +97,7 @@ public class PatternSet {
 				int attrValudId = checkRowIdxInPatternBitVec(attrId,
 						curAttrValue);
 				this.patternBitVecForCheckingDescendant[currentLevel][attrValudId]
-						.set(patternId);
+						.add(patternId);
 			}
 		}
 	}
@@ -142,24 +145,27 @@ public class PatternSet {
 		if (patternToCheck.level <= minLevel)
 			return false;
 
-		BitSet match = new BitSet(
-				patternBitVecForCheckingAncestorVectorLength[patternToCheck.level]);
-		match.set(0,
-				patternBitVecForCheckingAncestorVectorLength[patternToCheck.level]);
+		RoaringBitmap match = new RoaringBitmap();
+		match.add((long)0, patternBitVecForCheckingAncestorVectorLength[patternToCheck.level]);
 
 		// Only check attribute where value is 'x' in patternToCheck
 		for (int attrId = 0; attrId < patternToCheck.getDimension(); attrId++) {
 			char attrValueToCheck = patternToCheck.data[attrId];
-
-			BitSet bitVecForThisValueAtThisAttr = (BitSet) this.patternBitVecForCheckingAncestor[patternToCheck.level][checkRowIdxInPatternBitVec(
-					attrId, attrValueToCheck)].clone();
+			
 			if (attrValueToCheck != 'x') {
-				BitSet bitVecOfXAtThisAttr = this.patternBitVecForCheckingAncestor[patternToCheck.level][checkRowIdxOfXInPatternBitVec(
-						attrId)];
-				bitVecForThisValueAtThisAttr.or(bitVecOfXAtThisAttr);
-			}
 
-			match.and(bitVecForThisValueAtThisAttr);
+				RoaringBitmap bitVecForThisValueAtThisAttr = this.patternBitVecForCheckingAncestor[patternToCheck.level][checkRowIdxInPatternBitVec(
+						attrId, attrValueToCheck)];
+					RoaringBitmap bitVecOfXAtThisAttr = this.patternBitVecForCheckingAncestor[patternToCheck.level][checkRowIdxOfXInPatternBitVec(
+							attrId)];
+				match.and(RoaringBitmap.or(bitVecForThisValueAtThisAttr, bitVecOfXAtThisAttr));
+			}
+			else {
+				RoaringBitmap bitVecForThisValueAtThisAttr = this.patternBitVecForCheckingAncestor[patternToCheck.level][checkRowIdxInPatternBitVec(
+						attrId, attrValueToCheck)];
+		
+				match.and(bitVecForThisValueAtThisAttr);
+			}
 			if (match.isEmpty())
 				return false;
 
@@ -187,21 +193,25 @@ public class PatternSet {
 		if (patternToCheck.level >= maxLevel)
 			return false;
 
-		BitSet match = new BitSet(
-				patternBitVecForCheckingDescendantVectorLength[patternToCheck.level]);
-		match.set(0,
-				patternBitVecForCheckingDescendantVectorLength[patternToCheck.level]);
-
+//		RoaringBitmap match = new RoaringBitmap();
+//		match.add((long)0,(long)
+//				patternBitVecForCheckingDescendantVectorLength[patternToCheck.level]);
+		
+		List<RoaringBitmap> toAndBitVecs = new ArrayList<RoaringBitmap>();
+		
+		
+		
 		for (int attrId = 0; attrId < patternToCheck.getDimension(); attrId++) {
 			char attrValueToCheck = patternToCheck.data[attrId];
 			if (attrValueToCheck != 'x') {
-				match.and(
+				toAndBitVecs.add(
 						this.patternBitVecForCheckingDescendant[patternToCheck.level][checkRowIdxInPatternBitVec(
 								attrId, attrValueToCheck)]);
-				if (match.isEmpty())
-					return false;
+
 			}
 		}
+		
+		RoaringBitmap match = FastAggregation.and(toAndBitVecs.toArray(new RoaringBitmap[toAndBitVecs.size()]));
 
 		return !match.isEmpty();
 	}
@@ -211,6 +221,24 @@ public class PatternSet {
 	}
 
 	public static void main(String[] argv) {
+		RoaringBitmap r = new RoaringBitmap();
+		long s0 = System.currentTimeMillis();
+		for (int i = 0; i < 1000000; i++) {
+			RoaringBitmap r0 = new RoaringBitmap();
+			r0.add((long)0,i);
+			r.and(r0);
+		}
+		System.out.println(System.currentTimeMillis() - s0);
+		
+		
+		BitSet r1 = new BitSet();
+		long s1 = System.currentTimeMillis();
+		for (int i = 0; i < 1000000; i++) {
+			BitSet r0 = new BitSet();
+			r0.set(0,i);
+			r1.and(r0);
+		}
+		System.out.println(System.currentTimeMillis() - s1);
 	}
 
 }
