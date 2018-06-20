1366b19d883b4b3bf67b12c52cffbc0d31813947
Zhongjun Jin <markjin1990@gmail.com>
2018-06-20 18:21:00 -0400

    fixed abol's bug

--- scripts/testDC.sh
+++ scripts/testDC.sh
@@ -1,2 +1,2 @@
 cd ..;
-mvn compile exec:java@testdc -Dexec.args="-e -d 5 -n 100000 -t 200 -f data/airbnb_100000.csv  -o DC.text";
+mvn compile exec:java@testdc -Dexec.args="-d 5 -n 100000 -t 200 -f data/airbnb_100000.csv  -o";
