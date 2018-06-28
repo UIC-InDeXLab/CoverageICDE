5cdd79dfe09578163aa0c2bfa2a60db9afa86854
Zhongjun Jin <markjin1990@gmail.com>
2018-06-28 13:31:18 -0400

    modified dimension tests

--- src/test/java/vldb/DimensionTest.java
+++ src/test/java/vldb/DimensionTest.java
@@ -66,7 +66,7 @@ public class DimensionTest {
 				17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41};
 		int[] cardinalities = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
 				2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
-		int[] dimensions = new int[]{5, 10, 15, 20};
+		int[] dimensions = new int[]{5, 10, 15};
 
 		List<Map<String, String>> outputTestResultRecords = new ArrayList<Map<String, String>>();
 		String outputFileName = genFileName(cmd);
