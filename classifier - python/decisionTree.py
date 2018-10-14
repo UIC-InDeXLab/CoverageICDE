33836ed4af5bee039a032af1cf9285315c529b43
Zhongjun Jin <markjin1990@gmail.com>
2018-10-14 17:37:45 -0400

    updated decision tree

--- classifier - python/decisionTree.py
+++ classifier - python/decisionTree.py
@@ -2,6 +2,9 @@ from sklearn import tree
 import numpy as np
 from sklearn.metrics import accuracy_score
 import graphviz 
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import classification_report
+from sklearn.decomposition import TruncatedSVD
 
 
 def read_csv(filename):
@@ -9,28 +12,79 @@ def read_csv(filename):
 	f.readline()  # skip the header
 	data = np.loadtxt(fname = f, delimiter = ',')
 
-	varibles = data[:, 0:4]
-	label = data[:,4]
+	X = data[:, 0:4]
+	y = data[:,4]
 
-	return varibles, label
+	return X, y
 
 folder = 'data3/'
-filenames = ['RecidivismData_none.csv', 'RecidivismData_others.csv']
+
+whole_train_filename = 'RecidivismData_rand.csv'
+whole_test_filename = 'testRand.csv'
+X_test_whole, y_test_whole = read_csv(folder+whole_test_filename)
+
+filenames = ['RecidivismData_none.csv', 'RecidivismData_20.csv', 'RecidivismData_40.csv', 'RecidivismData_60.csv', 'RecidivismData_others.csv']
 testfile = 'TestR2.csv'
-test_variables, test_label = read_csv(folder+testfile)
+X_test_orig, y_test_orig = read_csv(folder+testfile)
+
+
+# One hot encoding
+X_train_enc, y_train_enc = read_csv(folder + whole_train_filename)
+enc = OneHotEncoder(handle_unknown='ignore')
+enc.fit(X_train_enc)
+X_train_enc = enc.transform(X_train_enc)
+
+# # PCA
+# pca = TruncatedSVD(n_components=8)
+# pca.fit(X_train_enc)
+
 
 for fn in filenames:
-	train_variables, train_label = read_csv(folder + fn)
+	X_train, y_train = read_csv(folder + fn)
 
 	clf = tree.DecisionTreeClassifier()
-	clf = clf.fit(train_variables, train_label)
-	y_pred = clf.predict(test_variables)
+
+
+	X_train = enc.transform(X_train).toarray()
+	# X_train = pca.transform(X_train)
+
+	# Training
+	clf = clf.fit(X_train, y_train)
+
+	# One-hot encoding
+	X_test = enc.transform(X_test_orig).toarray()
+	# X_test = pca.transform(X_test)
+	
+	# Testing
+	y_pred = clf.predict(X_test)
 	print fn
-	print "predict:", y_pred
-	print "actual:", test_label
-	print "accuracy", accuracy_score(test_label, y_pred)
+	print classification_report(y_test_orig, y_pred)
 	print 
 
-	dot_data = tree.export_graphviz(clf, out_file=None, feature_names=['sex','age','race','marrital status'], class_names={0: 'no', 1: 'yes'}) 
-	graph = graphviz.Source(dot_data) 
-	graph.render(fn) 
+	# dot_data = tree.export_graphviz(clf, out_file=None, feature_names=['sex','age','race','marrital status'], class_names={0: 'no', 1: 'yes'}) 
+	# graph = graphviz.Source(dot_data) 
+	# graph.render(fn) 
+
+
+
+# Use the whole dataset
+X_train, y_train = read_csv(folder + whole_train_filename)
+
+clf = tree.DecisionTreeClassifier()
+
+
+X_train = enc.transform(X_train).toarray()
+# X_train = pca.transform(X_train)
+
+# Training
+clf = clf.fit(X_train, y_train)
+
+# One-hot encoding
+X_test = enc.transform(X_test_whole).toarray()
+# X_test = pca.transform(X_test)
+
+# Testing
+y_pred = clf.predict(X_test)
+print whole_train_filename
+print classification_report(y_test_whole, y_pred)
+print 
--- classifier - python/svm.py
+++ classifier - python/svm.py
@@ -12,9 +12,9 @@ def read_csv(filename):
 
 	return varibles, label
 
-folder = 'data2/'
-filenames = ['RecidivismData_sub_none.csv', 'RecidivismData_sub.csv']
-testfile = 'TestR.csv'
+folder = 'data3/'
+filenames = ['RecidivismData_none.csv', 'RecidivismData_20.csv', 'RecidivismData_40.csv', 'RecidivismData_60.csv', 'RecidivismData_others.csv']
+testfile = 'TestR2.csv'
 test_variables, test_label = read_csv(folder+testfile)
 
 for fn in filenames:
