512ff2d1d8f034e2dd5dd7be9bd1833fc083b3f2
Zhongjun Jin <markjin1990@gmail.com>
2018-10-14 14:06:07 -0400

    machine learning with sklearn python library

---
+++ classifier - python/svm.py
@@ -0,0 +1,27 @@
+from sklearn.svm import SVC
+import numpy as np
+from sklearn.metrics import accuracy_score
+
+def read_csv(filename):
+	f = open(filename)
+	f.readline()  # skip the header
+	data = np.loadtxt(fname = f, delimiter = ',')
+
+	varibles = data[:, 0:3]
+	label = data[:,4]
+
+	return varibles, label
+
+folder = 'data2/'
+filenames = ['RecidivismData_sub_none.csv', 'RecidivismData_sub.csv']
+testfile = 'TestR.csv'
+test_variables, test_label = read_csv(folder+testfile)
+
+for fn in filenames:
+	train_variables, train_label = read_csv(folder + fn)
+
+	clf = SVC(gamma='auto')
+	clf = clf.fit(train_variables, train_label)
+	y_pred = clf.predict(test_variables)
+	print fn
+	print "accuracy", accuracy_score(test_label, y_pred)
---
+++ src/test/java/simpleTest/BlueNileMups.java
@@ -0,0 +1,72 @@
+package simpleTest;
+
+import java.util.Arrays;
+import java.util.Set;
+
+import io.DataSet;
+import pattern.Pattern;
+import search.HybridSearch;
+import search.NaiveSearch;
+import utils.Plot;
+
+public class BlueNileMups {
+	public static void main(String[] args) {
+		String fileName = "data/BlueNile_categorical.csv";
+		int[] chosenAttributeIds = {1, 2, 3, 4, 5, 6, 7};
+		int[] cardinalities = {10, 7, 4, 8, 3, 3, 5};
+		int d = 7;
+
+		double thresholdRate = 0.00001;
+
+		DataSet dataToCheck = new DataSet(fileName,
+				Arrays.copyOfRange(cardinalities, 0, d),
+				Arrays.copyOfRange(chosenAttributeIds, 0, d), 1000000);
+		
+		int threshold = (int) (thresholdRate * dataToCheck.getNumRecords());
+
+		// Test 1 with pattern breaker
+		HybridSearch ss = new HybridSearch(dataToCheck);
+
+		long t0 = System.currentTimeMillis();
+		Set<Pattern> mups = ss.findMaxUncoveredPatternSet(threshold);
+		long t1 = System.currentTimeMillis();
+
+		String breakline = String.format("%0" + 50 + "d", 0).replace("0", "-");
+
+		System.out.println(breakline);
+		System.out.println("Algo: HybridRandomSearch");
+		System.out.println("# of MUPs: " + mups.size());
+		System.out.println("Total Time: " + (t1 - t0) + " ms");
+		System.out.println("Visited: "
+				+ ss.getDebugInfo().get(NaiveSearch.DEBUG_NODES_VISITED));
+		System.out.println("Hits: " + ss.getNumHits());
+		
+		System.out.println(mups);
+
+
+		// Plot
+//		Plot pl = new Plot();
+//		pl.create2dPlot(ss.getTimeSeries());
+//		pl.setVisible(true);
+
+		
+		long[] levelInfo = new long[mups.size()];
+		
+		int[] count = new int[d+1];
+		
+		int i = 0;
+		for (Pattern p : mups) {
+			levelInfo[i++] = p.getLevel();
+			count[p.getLevel()]++;
+		}
+		
+		for (int j = 0; j < d; j++) {
+			System.out.println(j + "," + count[j]);
+		}
+
+		Plot pl2 = new Plot();
+		pl2.createBarchart(levelInfo, d);
+		pl2.setVisible(true);
+	}
+
+}
